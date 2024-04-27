7f24692dbb1b2fb9c419850147896ab9b9d66fcf
Raisa <volovik.raisa.kam@gmail.com>
2024-04-27 12:13:46 +0300

    added swagger

--- minimal_api/minimal_api/Program.cs
+++ minimal_api/minimal_api/Program.cs
@@ -1,13 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
-
-
-
 //app.UseHttpsRedirection();
 var hotels = new List<Hotel>();
 
